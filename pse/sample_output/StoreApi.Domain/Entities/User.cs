d684048ea3e7e603a156bc99f416ccd97c83f2cf
Ilija Spasić <spasicilija1@gmail.com>
2026-07-05 16:19:21 +0200

    chore: update sample output

--- pse/sample_output/StoreApi.Domain/Entities/User.cs
+++ pse/sample_output/StoreApi.Domain/Entities/User.cs
@@ -5,5 +5,5 @@ namespace StoreApi.Domain.Entities;
 public class User
 {
     public Guid Id { get; set; }
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 }
